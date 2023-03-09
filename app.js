7c65709ca95922070e5cbf577e52708edad2e3ad
[Sankhadip-Roy] <[sankhadiproy23@gmail.com]>
2023-03-09 20:18:06 +0530

    added more data

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
-// const API = `https://api.openweathermap.org/data/2.5/weather?
-// q=${city}&appid=${API_KEY}&units=metric`
-// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-weather.innerHTML = `<h2> Waiting for your input... <h2>`
+weather.innerHTML = `<h5> Waiting for your input... <h5>`
 const getWeather = async (city) => {
-    weather.innerHTML = `<h2> Loading... <h2>`
+    weather.innerHTML = `<h5> Loading... <h5>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url);
     const data = await response.json()
@@ -27,8 +24,40 @@ const showWeather = (data) => {
         <div>
             <h2>${data.main.temp} ℃</h2>
             <h4> ${data.weather[0].main} </h4>
-            <h6>Humidity: ${data.main.humidity} g.m-3</h6>
-            <h6>Wind Speed: ${data.wind.speed} m/s</h6>
+            <table class="table table-hover table-borderless table-sm table-striped  table-responsive-sm">
+                <thead>
+                    <tr>
+                    <th scope="col">${data.name}</th>
+                    <th scope="col">Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Humidity</td>
+                    <td>${data.main.humidity} g.m-3</td>
+                    </tr>
+                    <tr>
+                    <td >Temp</td>
+                    <td>Max: ${data.main.temp_max} Min: ${data.main.temp_min} ℃</td>
+                    </tr>
+                    <tr>
+                    <td >Feel like</td>
+                    <td>${data.main.feels_like} ℃</td>
+                    </tr>
+                    <tr>
+                    <td>Wind Speed</td>
+                    <td>${data.wind.speed} m/s in ${data.wind.deg}°</td>
+                    </tr>
+                    <tr>
+                    <td >Pressure</td>
+                    <td>${data.main.pressure / 1013} atm</td>
+                    </tr>
+                    <tr>
+                    <td >Location</td>
+                    <td>${data.sys.country} | ${data.coord.lat},  ${data.coord.lon} </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     `
 }
